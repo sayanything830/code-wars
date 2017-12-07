10912a5ae556b91546d59ed8ed631388fc1d27bb
Melanie Downing <sayanything830@gmail.com>
2017-12-07 14:31:24 -0800

    solved challenge and wrote up readme

---
+++ reversed-string/solution.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function solution(str){
+  return str.split('').reverse().join('');
+};
+
+// .split('') - separate the string by each letter
+// .reverse() - reverse the order of the elements in the string
+// .join('') - join the letters again into a string (opposite od split)
