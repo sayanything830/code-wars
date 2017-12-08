16ba3dad368d033e36deb2cb76991c607751bcc1
Melanie Downing <sayanything830@gmail.com>
2017-12-07 19:01:08 -0800

    solved without reverse method

--- reversed-string/solution.js
+++ reversed-string/solution.js
@@ -1,9 +1,25 @@
 'use strict';
 
+//Reversed String
+
+//without .reverse()
+function solution(str){ // make a function
+  let rev = []; // reserve empty array
+  str = str.split(''); // split each letter in string
+  for (let i = str.length -1; i >= 0; i --) { // for each letter in the string, starting at the end and decrementing to the beginning
+    rev.push(str[i]); // push that letter into the empty array
+  };
+  return rev.join(''); // join the array into a string
+}
+
+
+
+
+// with .reverse()
 function solution(str){
   return str.split('').reverse().join('');
 };
 
 // .split('') - separate the string by each letter
 // .reverse() - reverse the order of the elements in the string
-// .join('') - join the letters again into a string (opposite od split)
+// .join('') - join the letters again into a string (opposite of split)
