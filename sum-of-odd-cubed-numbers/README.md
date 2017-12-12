9ed691843f878a25fb5bd63d57f830e60beaa61b
Melanie Downing <sayanything830@gmail.com>
2017-12-11 21:36:04 -0800

    finished solution and readme

--- sum-of-odd-cubed-numbers/README.md
+++ sum-of-odd-cubed-numbers/README.md
@@ -1,5 +1,7 @@
 ## Sum of Odd Cubed Numbers
 
+This challenge wants a function that cubes and returns the sum of only the odd numbers in an array. If the array is empty or contains a data type other than an integer, it should return undefined.
+
 > Find the sum of the odd numbers within an array, after cubing the initial integers. This function will return undefined
 
 https://www.codewars.com/kata/sum-of-odd-cubed-numbers/javascript
--- sum-of-odd-cubed-numbers/solution.js
+++ sum-of-odd-cubed-numbers/solution.js
@@ -1,27 +1,25 @@
 'use strict';
 
-function cubeOdd(arr) {
+function cubeOdd(arr) { //make a function
 
-  for (i in arr) {
-   if (!Number.isInteger(arr[i])) {
-      return undefined;
+  for (i in arr) { //for each item in array
+   if (!Number.isInteger(arr[i])) { //if it's not a number
+      return undefined; //return undefined;
     }
   }
-  let isNumber = function (obj) {
-    return obj!== undefined && typeof(obj) === 'number' && !isNaN(obj);
+  let isNumber = function (obj) { //new function
+    return obj!== undefined && typeof(obj) === 'number' && !isNaN(obj); //returns only if elements are not undefined, a number data type, and is not NaN
   }
-  if (arr.filter(isNumber)) {
-    let odd = (num) => {
+  if (arr.filter(isNumber)) {// if this filter returns true
+    let odd = (num) => { //new function to return odd numbers
       return num % 2 !== 0;
-      // console.log(num)
   }
-    let oddArr = arr.filter(odd);
+    let oddArr = arr.filter(odd);//filter array with odd number filter function
 
-    if (oddArr.length === 0) {
-      return undefined;
+    if (oddArr.length === 0) { //if the array is empty
+      return undefined; //return undefined
     }
-    console.log(oddArr)
-    let cubed = oddArr.map(x => x * x * x);
-    return cubed.reduce((acc,curr) => acc + curr);
+    let cubed = oddArr.map(x => x * x * x); //otherwise cube the values of each and assign to new array named cubed
+    return cubed.reduce((acc,curr) => acc + curr); //return the sum of the value from each element in the cubed array
   }
 }
